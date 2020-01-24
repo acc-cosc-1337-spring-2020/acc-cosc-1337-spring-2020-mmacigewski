5c1003a2f77f899c8ee49559fdb5f20184972935
garrows69 <32017130+garrows69@users.noreply.github.com>
2020-01-23 18:51:47 -0600

    Added Hello World to main.cpp, commented out using std::cout... prefer to call to standard library within lines.

--- src/examples/01_module/01_output/main.cpp
+++ src/examples/01_module/01_output/main.cpp
@@ -1,9 +1,12 @@
 #include "output.h"
 
+#include <iostream>
+
+//using std::cout; //tells the compiler that we are using the cout from the standard lbrary
 
 int main() 
 {
-	
+	std::cout << "Hello World!" << std::endl;
 
 	return 0;
 }
