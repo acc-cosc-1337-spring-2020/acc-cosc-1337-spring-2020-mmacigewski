a3c53531c13175559ad769cd1969479450382aaf
mmacigewski <max.macigewski@g.austincc.edu>
2020-01-28 17:46:05 -0600

    Corrected lines and pushed from correct account

--- src/classwork/01_assign/main.cpp
+++ src/classwork/01_assign/main.cpp
@@ -8,15 +8,16 @@
 
 int main() 
 {
-	int iNum1, iNum2;
-
-	std::cout << "Input number 1: ";
+	int iNum1, iNum2; //11- 19 implemented by mmacigewski, pushed by garrows69(wrong account)
 	std::cin >> iNum1;
-	std::cout << "Input number 2: ";
+
+	std::cout << "Input number 1: ";  
+	std::cout << "Input number 2: ";  
 	std::cin >> iNum2;
 
 	std::cout << "\nSum of numbers: " << sum_numbers(iNum1, iNum2) << std::endl;
 	std::cout << "Product of numbers: " << multiply_numbers(iNum1, iNum2) << std::endl;
+
 	return 0;
 }
 
--- src/classwork/01_assign/types.cpp
+++ src/classwork/01_assign/types.cpp
@@ -1,12 +1,12 @@
 //Write the include statement for types.h here
 #include "types.h"
 
-int sum_numbers(int num1, int num2)
+int sum_numbers(int num1, int num2) 
 {
 	return num1 + num2;
 }
 
-int multiply_numbers(int num1, int num2) //added due to comment on line 1 within "types.h"
+int multiply_numbers(int num1, int num2) //added due to comment on line 1 within "types.h" //9-12 implemented by mmacigewski, pushed by garrows69(wrong account)
 {
 	return num1 * num2; 
 }
