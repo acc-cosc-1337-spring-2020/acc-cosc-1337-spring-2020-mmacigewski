44f8b8cac9df0ea538630c02baa8fb31a977539e
mmacigewski <max.macigewski@g.austincc.edu>
2020-01-28 19:22:39 -0600

    Added unit test for multiply_numbers() Included the in class examples, and then the homework requirements

--- test/homework_test/01_expressions_test/01_expressions_tests.cpp
+++ test/homework_test/01_expressions_test/01_expressions_tests.cpp
@@ -12,3 +12,15 @@ TEST_CASE("Verify sum_numbers function")
 	REQUIRE(add_numbers(10, 10) == 20);
 }
 
+TEST_CASE("Verify multiply_numbers function")
+{
+	//in class unit test
+	REQUIRE(multiply_numbers(2, 2) == 4);
+	REQUIRE(multiply_numbers(3, 3) == 9);
+	REQUIRE(multiply_numbers(5, 5) == 25);
+
+	//homework unit test
+	REQUIRE(multiply_numbers(2, 4) == 8);
+	REQUIRE(multiply_numbers(3, 5) == 15);
+	REQUIRE(multiply_numbers(10, 10) == 100);
+}
