825f93bf200e88b47227e375cac2619e5133f542
mmacigewski <max.macigewski@g.austincc.edu>
2020-02-06 20:41:51 -0600

    Added space in lines of code to make it look pretty.

--- src/classwork/02_assign/decision.cpp
+++ src/classwork/02_assign/decision.cpp
@@ -5,10 +5,12 @@ double gross_pay(double hours, double hourly_rate)
 {
 	const int IMAXHOURS = 40;
 	const double OTRATE = 1.5;
+
 	if (hours > IMAXHOURS)
 	{
 		int OT = hours - IMAXHOURS;
 		return (((hours - OT) * hourly_rate) + (hourly_rate * OTRATE * OT));
 	}
+
 	else { return hours * hourly_rate; }
 }
--- src/classwork/02_assign/main.cpp
+++ src/classwork/02_assign/main.cpp
@@ -11,15 +11,18 @@ int main()
 	//create a double variable named hours
 	//create a double variable named hourly_rate
 	double hours, hourly_rate, gross_salary;
+
 	//Prompt user for keyboard input for hours and hourly_rate
 	//save the input to variables hours and hourly_rate
 	cout << "Input hours: ";
 	cin >> hours;
 	cout << "Input hourly rate: ";
 	cin >> hourly_rate;
+
 	//Create a double variable named gross_salary
 	//Call the gross_pay function and save its return value to the gross_salary variable
 	gross_salary = gross_pay(hours, hourly_rate);
+
 	//Display the gross_salary variable as follows:
 	//Example ---->>>     Gross pay: 150.00   
 	cout << "Gross pay: " << gross_salary;
