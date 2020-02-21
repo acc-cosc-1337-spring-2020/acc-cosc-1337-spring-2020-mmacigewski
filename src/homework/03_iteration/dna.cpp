22e744b738bd459e57f8f4c28f711843075b5183
mmacigewski <max.macigewski@g.austincc.edu>
2020-02-21 15:25:58 -0600

    Completed the homework. wrote test cases as well.

--- src/homework/03_iteration/dna.cpp
+++ src/homework/03_iteration/dna.cpp
@@ -1,4 +1,5 @@
 #include "dna.h"
+#include <iostream>
 /*
 Write code for function get_gc_content that accepts
 a const reference string parameter and returns a double.
@@ -6,15 +7,39 @@ Calculate GC content:
 Iterate string count Gs and Cs, divide count by string length.
 Return quotient.
 */
+double get_gc_content(const std::string& sDNA)
+{
+    double dGCCount = 0.0;
 
+    for (int i = 0; i < sDNA.length(); ++i)
+    {
+        if (sDNA[i] == 'C' || sDNA[i] == 'G')
+        {
+            dGCCount++;
+        }
+    }
 
+    return dGCCount / sDNA.length();
+}
 
 
 /*
 Write code for function get_reverse_string that
 accepts a string parameter and returns a string reversed.
 */
+std::string get_reverse_string(std::string sDNA)
+{
+    std::string sNewString = sDNA; //sets the new string to proper length
+    int iStartIndex = 0;
 
+    for (int i = sDNA.length() - 1; i >= 0; i--)
+    {
+        sNewString[iStartIndex] = sDNA[i];
+        iStartIndex++;
+    }
+
+    return sNewString;
+}
 
 
 /*
@@ -27,4 +52,31 @@ b. iterate local string variable and
 c. return string
 
 */
+std::string get_dna_complement(std::string sDNA)
+{
+    std::string sDNAReverse = "";
 
+    sDNAReverse = get_reverse_string(sDNA);
+
+    for (int i = 0; i < sDNAReverse.length(); ++i)
+    {
+        if (sDNAReverse[i] == 'A')
+        {
+            sDNAReverse[i] = 'T';
+        }
+        else if (sDNAReverse[i] == 'T')
+        {
+            sDNAReverse[i] = 'A';
+        }
+        else if (sDNAReverse[i] == 'C')
+        {
+            sDNAReverse[i] = 'G';
+        }
+        else if (sDNAReverse[i] == 'G')
+        {
+            sDNAReverse[i] = 'C';
+        }
+    }
+
+    return sDNAReverse;
+}
--- src/homework/03_iteration/main.cpp
+++ src/homework/03_iteration/main.cpp
@@ -1,7 +1,9 @@
 //write include statements
+#include "dna.h"
 
+#include <iostream>
 //write using statements
-
+using std::cout; using std::cin;
 
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
@@ -12,5 +14,47 @@ user enters a y or Y.
 */
 int main() 
 {
+	bool bIsRunning = true;
+	int iOption = 0;
+	std::string sDNA = "";
+	char cKeepRun = ' ';
+
+	while (bIsRunning)
+	{
+		cout << "Option 1: Get GC Content";
+		cout << "\nOption 2: Get DNA Complement";
+		cout << "\nInput Option: ";
+		cin >> iOption;
+		
+		if (iOption == 1)
+		{
+			cout << "\nEnter DNA: ";
+			cin >> sDNA;
+
+			cout << "GC Content: " << get_gc_content(sDNA);
+		}
+		else if (iOption == 2)
+		{
+			cout << "\nEnter DNA: ";
+			cin >> sDNA;
+			
+			cout << "DNA Complement: " << get_dna_complement(sDNA);
+		}
+		else if (iOption == 3)
+		{
+			cin >> sDNA;
+			cout << get_reverse_string(sDNA);
+		}
+
+		cout << "\n\nDo you still want to continute?(Y/y)";
+		cin >> cKeepRun;
+
+		if (std::tolower(cKeepRun) == 'n')
+		{
+			cout << "Program Ending.";
+			bIsRunning = false;
+		}else { cout << "\n"; } //for formatting
+	}
+
 	return 0;
 }
