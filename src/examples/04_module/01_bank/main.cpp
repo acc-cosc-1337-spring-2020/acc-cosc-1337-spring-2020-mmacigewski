7a987cb6404e318fcf881e8ca48786069621546e
mmacigewski <max.macigewski@g.austincc.edu>
2020-02-25 19:06:41 -0600

    Follow up update to main

--- src/examples/04_module/01_bank/main.cpp
+++ src/examples/04_module/01_bank/main.cpp
@@ -2,17 +2,22 @@
 
 #include <iostream>
 
+using std::cin; using std::cout;
+
 int main()
 {
 	auto num{ 0 }; //automatically initializes num to 0 //THIS IS BEST PRACTICE
 
-	std::cout << "Input Balance: ";
-	std::cin >> num;
-
 	BankAccount account{ num }; //creates the class object, uses the industry standard
 
 	auto balance = account.iGetBalance(); //auto will give it balance an int type
-	std::cout << "Balance is: " << balance;
+	cout << "Balance is: " << balance;
+
+	auto amount{ 0 };
+	cout << "Enter deposit amount: ";
+	cin >> amount;
+
+	cout << "Balance is: " << balance;
 
 	return 0;
 }
