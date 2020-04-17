fec50d27721aedc1265c1048d0a9db0bb2ea14db
mmacigewski <max.macigewski@g.austincc.edu>
2020-04-16 19:02:37 -0500

    followed along in class.

--- src/examples/11_module/02_dynamic_memory/main.cpp
+++ src/examples/11_module/02_dynamic_memory/main.cpp
@@ -1,5 +1,18 @@
+#include <iostream>
+
+using std::cout;
+
 int main() 
 {
 	
+	int* ptr_num = new int(5); 
+
+	cout << "Address of ptr_num: " << &ptr_num << "\n";
+	cout << "Address ptr_num points to: " << ptr_num << "\n";
+	cout << "Value at adress ptr_num points to: " << *ptr_num;
+
+	delete ptr_num;
+	ptr_num = nullptr; //if we don't do this it is a memory leak.
+
 	return 0;
 }
--- src/examples/11_module/03_memory_leak/memory_leak.cpp
+++ src/examples/11_module/03_memory_leak/memory_leak.cpp
@@ -1,2 +1,9 @@
 #include "memory_leak.h"
 
+void memory_leak()
+{
+	int* ptr_num = new int(5);
+	//other code
+
+	//no delete causes a memory leak, so every time it is called, it takes up more and more memory
+}
